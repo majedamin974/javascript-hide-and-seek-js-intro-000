0b8ccac2f06a29496e851cde49572b5ba5a58e8f
Majed Amin <majedamin974@gmail.com>
2021-02-15 17:43:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  document.querySelector("#nested .target").remove();
+}
+
+function increaseRankBy(n) {
+  let list = document.querySelectorAll(".ranked-list > li");
+  for (var i = 0; i < list.length; i++) {
+    list[i].innerHTML = parseInt(list[i].innerHTML) + n;
+  }
+}
